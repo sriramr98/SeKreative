b3ecb84b3da90c7235d847faed311c1e090a24f6
Aayush005 <aayush.upadhyay9@gmail.com>
2019-01-23 02:18:14 +0530

    Bottom AppBAr Implemented.

--- app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
+++ app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 
 import androidx.fragment.app.Fragment
 import com.sekreative.sekreative.R
-
+import com.sekreative.sekreative.ui.addpost.AddPostFragment
+import com.sekreative.sekreative.ui.auth.AuthActivity
+import com.sekreative.sekreative.ui.feed.FeedFragment
+import com.sekreative.sekreative.ui.profile.ProfileFragment
 import com.sekreative.sekreative.utils.Prefs
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentFragment = FeedFragment.TAG
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
 
+    }
 }
