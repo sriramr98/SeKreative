52a1ff0b37aa71fd602fe8f19ed9bf36433f3184
Aayush005 <aayush.upadhyay9@gmail.com>
2019-01-22 23:01:03 +0530

    Created a workable dev branch.

--- app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
+++ app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
@@ -2,34 +2,26 @@ package com.sekreative.sekreative.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
+
 import com.sekreative.sekreative.R
-import com.sekreative.sekreative.ui.auth.AuthActivity
+
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val mAuth = FirebaseAuth.getInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_logout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity<AuthActivity>()
+
             finish()
             return@setOnClickListener
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (mAuth.currentUser == null) {
-            startActivity<AuthActivity>()
-            finish()
-            return
-        }
-    }
+
 }
