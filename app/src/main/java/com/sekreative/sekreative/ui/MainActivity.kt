8aa5e42a118c84d6991c6c947c77fde3063906bd
Aayush005 <aayush.upadhyay9@gmail.com>
2019-01-23 01:55:30 +0530

    Bottom AppBAr Implemented.

--- app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
+++ app/src/main/java/com/sekreative/sekreative/ui/MainActivity.kt
@@ -3,25 +3,19 @@ package com.sekreative.sekreative.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.fragment.app.Fragment
 import com.sekreative.sekreative.R
 
+import com.sekreative.sekreative.utils.Prefs
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_logout.setOnClickListener {
-
-            finish()
-            return@setOnClickListener
-        }
     }
-
 
 }
